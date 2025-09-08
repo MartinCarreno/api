1ac1b2876b48dde80b40a44bbee1fa00f1aa3503
Martin Carreno <martincarreno71@gmail.com>
2025-09-08 16:35:18 -0300

    Carga de SWAGGER

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { console } from 'inspector';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
+ const app = await NestFactory.create(AppModule, { cors: true });
+ app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true
+  }));
+ app.enableCors({
+ origin: 'http://localhost:5173',
+ credentials: true,
+ });
 
-console.log("SI");
+ const config = new DocumentBuilder()
+ .setTitle('TaskBoard API')
+ .setDescription('CRUD de tareas')
+ .setVersion('1.0')
+ .build();
+ const document = SwaggerModule.createDocument(app, config);
+ SwaggerModule.setup('api/docs', app, document);
+ 
+ await app.listen(3000);
+}
 bootstrap();

d748753130ed8e79d493b33ecd347d7b424bd810
Martin Carreno <martincarreno71@gmail.com>
2025-09-08 15:32:40 -0300

    si

---
+++ prisma/migrations/20250908173940_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "public"."Tipo" AS ENUM ('BEBESTIBLE', 'COMESTIBLE');
+
+-- CreateTable
+CREATE TABLE "public"."Producto" (
+    "id" TEXT NOT NULL,
+    "Nombre" TEXT NOT NULL,
+    "description" TEXT,
+    "tipo" "public"."Tipo" NOT NULL DEFAULT 'COMESTIBLE',
+    "dueDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Producto_pkey" PRIMARY KEY ("id")
+);
